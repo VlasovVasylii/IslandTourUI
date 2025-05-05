d371f836d4962ad58f19114bc197fd22ae4df704
Vasylii <dedkek56@gmail.com>
2025-05-06 00:03:15 +0600

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace IslandTourUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Output = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtSelectedFile = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(241, 27);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(139, 115);
+            this.Output.TabIndex = 0;
+            this.Output.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(66, 104);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 38);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Input_File";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(238, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Output";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 31);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Browse";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtSelectedFile
+            // 
+            this.txtSelectedFile.Location = new System.Drawing.Point(25, 27);
+            this.txtSelectedFile.Name = "txtSelectedFile";
+            this.txtSelectedFile.Size = new System.Drawing.Size(100, 20);
+            this.txtSelectedFile.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(403, 180);
+            this.Controls.Add(this.txtSelectedFile);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Output);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textInput;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox rtbOutput;
+        private System.Windows.Forms.Button Button;
+        private System.Windows.Forms.RichTextBox Output;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtSelectedFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace IslandTourUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            openFileDialog1.FileName = "input.txt";
+            txtSelectedFile.Text = openFileDialog1.FileName ;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(openFileDialog1.FileName))
+                {
+                    MessageBox.Show("Сначала выберите файл!");
+                    return;
+                }
+
+                string[] input = File.ReadAllLines(openFileDialog1.FileName);
+                var grid = Solution.ParseInput(input);
+                var path = Solution.GeneratePath(grid);
+
+                Output.Text = "";
+                foreach (var cell in path)
+                {
+                    Output.AppendText($"{cell.Item1 + 1} {cell.Item2 + 1}\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка: {ex.Message}");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.DefaultExt = ".txt";
+            openFileDialog1.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Обновляем интерфейс после выбора файла
+                txtSelectedFile.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+
+    static class Solution
+    {
+        static public char[][] ParseInput(string[] inputLines)
+        {
+            if (inputLines.Length < 2)
+                throw new ArgumentException("Недостаточно строк во входном файле");
+
+            int n = int.Parse(inputLines[0]);
+            int m = int.Parse(inputLines[1]);
+
+            if (inputLines.Length - 2 != n)
+                throw new ArgumentException("Несоответствие размеров сетки");
+
+            char[][] grid = new char[n][];
+            for (int i = 0; i < n; i++)
+            {
+                grid[i] = inputLines[i + 2].PadRight(m, '.').ToCharArray();
+            }
+            return grid;
+        }
+
+        static public List<Tuple<int, int>> GeneratePath(char[][] grid)
+        {
+            var neighboringCells = new HashSet<Tuple<int, int>>();
+
+            // 1. Находим все соседние клетки
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++) // Исправлено на grid[i]
+                {
+                    if (grid[i][j] == '.' && IsNeighborToIsland(grid, i, j))
+                    {
+                        neighboringCells.Add(Tuple.Create(i, j));
+                    }
+                }
+            }
+
+            if (neighboringCells.Count == 0)
+                return new List<Tuple<int, int>>();
+
+            // 2. Выбираем стартовую клетку
+            var startCell = neighboringCells
+                .OrderBy(c => c.Item1)
+                .ThenBy(c => c.Item2)
+                .First();
+
+            // 3. Строим путь
+            return BuildPath(neighboringCells, startCell);
+        }
+
+        private static bool IsNeighborToIsland(char[][] grid, int x, int y)
+        {
+            for (int dx = -1; dx <= 1; dx++)
+            {
+                for (int dy = -1; dy <= 1; dy++)
+                {
+                    if (dx == 0 && dy == 0) continue;
+
+                    int nx = x + dx;
+                    int ny = y + dy;
+
+                    // Исправленная проверка границ
+                    if (nx >= 0 && nx < grid.Length &&
+                        ny >= 0 && ny < grid[nx].Length &&
+                        grid[nx][ny] == '#')
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        private static List<Tuple<int, int>> BuildPath(
+            HashSet<Tuple<int, int>> cells,
+            Tuple<int, int> start)
+        {
+            var path = new List<Tuple<int, int>> { start };
+            var visited = new HashSet<Tuple<int, int>> { start };
+
+            // Направления: вправо, вниз, влево, вверх
+            int[][] directions = {
+                new[] { 0, 1 },  // right
+                new[] { 1, 0 },  // down
+                new[] { 0, -1 }, // left
+                new[] { -1, 0 } // up
+            };
+
+            int dirIndex = 0;
+            int retryCount = 0;
+
+            while (visited.Count < cells.Count)
+            {
+                var last = path.Last();
+                var newDir = directions[dirIndex];
+                var nextCell = Tuple.Create(last.Item1 + newDir[0], last.Item2 + newDir[1]);
+
+                if (cells.Contains(nextCell) && !visited.Contains(nextCell))
+                {
+                    path.Add(nextCell);
+                    visited.Add(nextCell);
+                    retryCount = 0;
+                }
+                else
+                {
+                    dirIndex = (dirIndex + 1) % 4;
+                    retryCount++;
+
+                    // Защита от бесконечного цикла
+                    if (retryCount > 4) break;
+                }
+            }
+
+            return path;
+        }
+    }
+}
